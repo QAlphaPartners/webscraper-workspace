944fa093acf1f6bcc528805f4e2fff8105f79193
Conan Chan <conan8chan@yahoo.com>
2023-07-20 16:48:51 +0800

    init commit

--- fund-eastmoney/js/rollup.config.js
+++ fund-eastmoney/js/rollup.config.js
@@ -1,6 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
+import commonjs from "@rollup/plugin-commonjs";
+import json from '@rollup/plugin-json';
+
 export default [
   {
     input: "./dist/inject.js",
@@ -9,6 +12,11 @@ export default [
       entryFileNames: "[name].min.js",
       format: "iife",
     },
-    plugins: [resolve(), terser({ format: { comments: false } })],
+    plugins: [resolve(), terser({ format: { comments: false } }), commonjs({
+      // include the playwright-core module
+      include: /node_modules\/.pnpm\/playwright-core/,
+      // specify how to handle default exports
+      requireReturnsDefault: "auto",
+    }), json()],
   },
 ];
