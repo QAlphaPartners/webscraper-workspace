f7b3484ea6e2f672c53e81608de2922c32ed7110
Conan Chan <conan8chan@yahoo.com>
2023-08-03 18:08:48 +0800

    init commit

--- plugins/tauri-plugin-jsinject/js/inject.ts
+++ plugins/tauri-plugin-jsinject/js/inject.ts
@@ -1,10 +1,21 @@
 
-import {  emit,  } from '@tauri-apps/api/event'
+import { emit, } from '@tauri-apps/api/event'
 
 declare var __DEBUG__: boolean;
 
+let previousUrl: string = "null";
+
 console.log("[tauri-plugin-jsinject/inject.ts] inject common js into webview with url ", window.location.href)
 window.addEventListener("DOMContentLoaded", async (event) => {
-  await emit("InjectInited", { loggedIn: true, token: '[inject.ts] init', type: event.type });
+
+  // Check if the current URL is different from the previous one
+  if (window.location.href !== previousUrl) {
+
+    await emit("InjectInited", { loggedIn: true, token: '[inject.ts] init', type: event.type, url: window.location.href });
+
+    // Update the previous URL
+    previousUrl = window.location.href;
+  }
+
 })
 
