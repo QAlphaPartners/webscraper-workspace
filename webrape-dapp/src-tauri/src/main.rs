e0824f650b5dda6a18890ffe57764101f9b085b8
Conan Chan <conan8chan@yahoo.com>
2023-08-08 18:31:18 +0800

    init commit

--- webrape-dapp/src-tauri/src/main.rs
+++ webrape-dapp/src-tauri/src/main.rs
@@ -41,22 +41,10 @@ async fn start_scrape<R: Runtime>(handle: AppHandle<R>, url: String) {
 
     for i in 0..MAX_CONCURRENT_SCRAPERS {
         let label = format!("Scraper_{}", i);
-        let label_ = label.clone();
         if let Some(_w) = handle_.app_handle().get_window(label.as_str()) {
         } else {
-            let url_ = url.clone();
-            let new_window = tauri::WindowBuilder::new(
-                &handle,
-                label,                             // the unique window label
-                tauri::WindowUrl::App(url.into()), // the url to load
-            )
-            .title(format!("[{}]Scraping...: {}", label_, url_))
-            .initialization_script(
-                &include_str!("../../dist-jslib/fund-eastmoney-scraper.min.js")
-                    .replace("__DEBUG__", &format!("{}", true)),
-            )
-            .build()
-            .unwrap();
+
+            let new_window = create_fund_eastmoney_window(url,handle,label);
 
             let w_ = new_window.clone();
             // --- listen event from frontend: 网页刮取的网址，格式json
@@ -66,67 +54,95 @@ async fn start_scrape<R: Runtime>(handle: AppHandle<R>, url: String) {
 
                 // get a reference to the payload object
                 let payload = event.payload().unwrap();
-                println!("got FataEvent with payload: {:?}\n", payload);
-                // try to deserialize it into your struct
-                match serde_json::from_str::<FataEvent<DataValue>>(payload) {
-                    Ok(fata_event) => {
-                        println!("got FataEvent with fata_event: {:?}\n", fata_event);
-                        // get the data field as an option of a vector of DataValue enums
-                        let data_values = fata_event.data;
-
-                        // match on the option of a vector of DataValue enums
-                        match data_values {
-                            Some(data_values) => {
-                                // use a for loop or an iterator to process each element of the vector
-                                for data_value in data_values {
-                                    // match on the variant of the DataValue enum
-                                    match data_value {
-                                        DataValue::StoreValue(store_value) => {
-                                            // do something with the store_value
-                                        }
-                                        DataValue::ShopValue(shop_value) => {
-                                            // do something with the shop_value
-                                        }
-                                        DataValue::ProductValue(product_value) => {
-                                            // do something with the product_value
-                                        }
-                                        DataValue::FundNetValue(fund_net_value) => {
-
-                                            println!("got FataEvent DataValue::FundNetValue(fund_net_value) {:?}\n",fund_net_value);
-                                            // Create a new object for the struct FataEvent using the new method
-                                            let e_: event::BomaEvent<String> =
-                                                event::BomaEvent::new(
-                                                    "some hub name".to_string(),
-                                                    "some topic name".to_string(),
-                                                    Some("some label".to_string()),
-                                                    Some("some data".to_string()),
-                                                );
-
-                                            w_.emit_all("BomaEvent", e_).unwrap();
-                                        }
-                                        DataValue::StringValue(string_value)=>{
-
-                                            println!("got FataEvent DataValue::StringValue(string_value) {:?}\n",string_value);
-
-                                        }
-                                    }
-                                }
-                            }
-                            None => {
-                                // handle the case when there is no data
-                                println!("got FataEvent None!!!!")
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        // handle the error
-                    }
-                }
-
+                process_fata_event(&w_, payload);
                 // base64_hello()
             });
 
             break;
+        }
+    }
+}
+
+fn create_fund_eastmoney_window<R: Runtime>(
+    url: String,
+    handle: AppHandle<R>,
+    label: String,
+) -> Window<R> {
+    let url_ = url.clone();
+    let label_ = label.clone();
+    let new_window = tauri::WindowBuilder::new(
+        &handle,
+        label,                             // the unique window label
+        tauri::WindowUrl::App(url.into()), // the url to load
+    )
+    .title(format!("[{}]Scraping...: {}", label_, url_))
+    .initialization_script(
+        &include_str!("../../dist-jslib/fund-eastmoney-scraper.min.js")
+            .replace("__DEBUG__", &format!("{}", true)),
+    )
+    .build()
+    .unwrap();
+    return new_window;
+}
+
+
+fn process_fata_event<R: Runtime>(w_: &Window<R>, payload: &str) -> () {
+    println!("got FataEvent with payload: {:?}\n", payload);
+    // try to deserialize it into your struct
+    match serde_json::from_str::<FataEvent<DataValue>>(payload) {
+        Ok(fata_event) => {
+            println!("got FataEvent with fata_event: {:?}\n", fata_event);
+            // get the data field as an option of a vector of DataValue enums
+            let data_values = fata_event.data;
+
+            // match on the option of a vector of DataValue enums
+            match data_values {
+                Some(data_values) => {
+                    // use a for loop or an iterator to process each element of the vector
+                    for data_value in data_values {
+                        // match on the variant of the DataValue enum
+                        match data_value {
+                            DataValue::StoreValue(store_value) => {
+                                // do something with the store_value
+                            }
+                            DataValue::ShopValue(shop_value) => {
+                                // do something with the shop_value
+                            }
+                            DataValue::ProductValue(product_value) => {
+                                // do something with the product_value
+                            }
+                            DataValue::FundNetValue(fund_net_value) => {
+                                println!(
+                                    "got FataEvent DataValue::FundNetValue(fund_net_value) {:?}\n",
+                                    fund_net_value
+                                );
+                                // Create a new object for the struct FataEvent using the new method
+                                let e_: event::BomaEvent<String> = event::BomaEvent::new(
+                                    "some hub name".to_string(),
+                                    "some topic name".to_string(),
+                                    Some("some label".to_string()),
+                                    Some("some data".to_string()),
+                                );
+
+                                w_.emit_all("BomaEvent", e_).unwrap();
+                            }
+                            DataValue::StringValue(string_value) => {
+                                println!(
+                                    "got FataEvent DataValue::StringValue(string_value) {:?}\n",
+                                    string_value
+                                );
+                            }
+                        }
+                    }
+                }
+                None => {
+                    // handle the case when there is no data
+                    println!("got FataEvent None!!!!")
+                }
+            }
+        }
+        Err(e) => {
+            // handle the error
         }
     }
 }
